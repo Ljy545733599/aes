d10577b5da447a2bc94786207980e3bbcd278e8a
林俊毅 <linjunyi02@corp.netease.com>
2022-01-26 14:46:38 +0800

    init

---
+++ app/src/main/cpp/NEAES.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wb.linjunyi on 2021/7/29.
+//
+
+#include "NEAES.h"
+#include <jni.h>
+
+std::string neaes::NEAES::getAESCode() {
+    return "KKsDuFICsL2WrCeR";
+}
---
+++ app/src/main/cpp/NEAES.h
@@ -0,0 +1,17 @@
+//
+// Created by wb.linjunyi on 2021/7/29.
+//
+
+#ifndef APP_NEAES_H
+#define APP_NEAES_H
+
+#include <string>
+
+namespace neaes{
+    class NEAES{
+        public:
+            static std::string getAESCode();
+    };
+}
+
+#endif //APP_NEAES_H
